a40bf57feca8f5affbd36f916d5d5b088c618267
JianyueLab <valor@jianyuelab.org>
2025-02-13 18:25:08 +0900

    Feat: GET & POST & PUT & DELETE

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -1,11 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import {NextApiRequest, NextApiResponse} from 'next';
 import pool from "@/lib/db";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // GET DATA
+  // GET DATA
   if (req.method === "GET") {
     try {
-      // const the data data
       const [data] = await pool.query("SELECT * FROM anonymous");
       res.status(200).json({
         status: 200,
@@ -13,27 +12,67 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ status: 500, message: "Internal Server Error" });
+      res.status(500).json({status: 500, message: "Internal Server Error"});
     }
-    // POST DATA
-  }  else if (req.method === "POST") {
+
+    // POST
+  } else if (req.method === "POST") {
     try {
-      const { question, answer } = req.body;
+      const {question, answer} = req.body;
 
       if (!question) {
-        return res.status(400).json({ message: "Missing required fields" });
+        return res.status(400).json({message: "Missing required fields"});
       }
 
       const query = "INSERT INTO anonymous (question, answer) VALUE (?,?)"
       const values = [question, answer];
 
       await pool.query(query, values);
-      res.status(200).json({ message: "Question added successfully", question: question });
+      res.status(200).json({message: "Question added successfully", question: question});
     } catch (error) {
-      console.error("Database Error:", error);
+      console.error(error);
       res.status(500).json({message: "Internal Server Error"});
     }
+
+    // UPDATE
+  } else if (req.method === "PUT") {
+    try {
+      const {id, answer} = req.body;
+
+      if (!id || !answer) {
+        return res.status(400).json({message: "Missing required fields"});
+      }
+
+      const query = "UPDATE anonymous SET answer = ? WHERE id = ?"
+      const values = [answer, id];
+
+      await pool.query(query, values);
+      res.status(200).json({message: "Answer Updated", id: id})
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({message: "Internal Server Error"});
+    }
+
+    // DELETE
+  } else if (req.method === "DELETE") {
+    try {
+      const {id} = req.body;
+
+      if (!id) {
+        return res.status(400).json({message: "Missing required fields"});
+      }
+
+      const query = "DELETE FROM anonymous WHERE id = ?"
+      const value = [id];
+
+      await pool.query(query, value);
+      res.status(200).json({message: "Deleted successfully", id: id})
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({message: "Internal Server Error"});
+    }
+
   } else {
-    res.status(405).json({ status: 405, message: "Method doesn't allow" });
+    res.status(405).json({status: 405, message: "Method doesn't allow"});
   }
 }
