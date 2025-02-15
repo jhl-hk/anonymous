aa806511ee1ef4f592f9949d3c3b932e85ebfb5b
JianyueLab <valor@jianyuelab.org>
2025-02-15 20:01:43 +0900

    Refactor: data[0]

--- src/pages/api/question/[id].ts
+++ src/pages/api/question/[id].ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(404).json({ message: "Question not found" });
       }
 
-      res.status(200).json(data);
+      res.status(200).json(data[0]);
     } catch (error) {
       console.error(error);
       res.status(500).json({message: "Internal Server Error"});
