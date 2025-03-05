160086854667174e44e366f076fab15e925b4e0f
JianyueLab <valor@jianyuelab.org>
2025-03-05 09:44:46 +0900

    Fix: login variables error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     // ...add more providers here
   ],
   callbacks: {
-    async signIn({user}) {
+    async signIn({ user }) {
       // fetch user email
       const userEmail = user.email;
 
@@ -21,22 +21,21 @@ export const authOptions = {
         return false;
       }
 
-      const GITHUB_EMAIL_WHITELIST = process.env.GITHUB_EMAIL_WHITELIST;
+      const GITHUB_EMAIL_WHITELIST = process.env.GITHUB_EMAIL_WHITELIST || "";
 
       // check with environmental variables
-      if (GITHUB_EMAIL_WHITELIST?.includes(userEmail)) {
+      if (GITHUB_EMAIL_WHITELIST.includes(userEmail)) {
         return true;
       }
 
       // refuse login
       return false;
     },
-
-    pages: {
-      error: "/", // 当用户被拒绝登录时重定向到此页面
-      signOut: "/", // 自定义退出登录页面
-    },
-  }
+  },
+  pages: {
+    error: "/", // 当用户被拒绝登录时重定向到此页面
+    signOut: "/", // 自定义退出登录页面
+  },
 }
 
 export default NextAuth(authOptions)
