ccc8ad55ae486b88a79c5fd3919e330c997d091e
JianyueLab <valor@jianyuelab.org>
2025-03-05 09:14:16 +0900

    Feat: Whitelist for login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,6 +10,33 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({user}) {
+      // fetch user email
+      const userEmail = user.email;
+
+      // if no email
+      if (!userEmail) {
+        console.log("GitHub 用户没有提供邮箱，拒绝登录");
+        return false;
+      }
+
+      const GITHUB_EMAIL_WHITELIST = process.env.GITHUB_EMAIL_WHITELIST;
+
+      // check with environmental variables
+      if (GITHUB_EMAIL_WHITELIST?.includes(userEmail)) {
+        return true;
+      }
+
+      // refuse login
+      return false;
+    },
+
+    pages: {
+      error: "/", // 当用户被拒绝登录时重定向到此页面
+      signOut: "/", // 自定义退出登录页面
+    },
+  }
 }
 
 export default NextAuth(authOptions)
