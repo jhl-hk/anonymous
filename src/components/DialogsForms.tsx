acd0d1db61279144af5d241733d1138de28945e8
JianyueLab <valor@jianyuelab.org>
2025-03-17 10:10:22 +0900

    Fix: Button display

--- src/components/DialogsForms.tsx
+++ src/components/DialogsForms.tsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from "react";
 import {Dialog, DialogBackdrop, DialogPanel, DialogTitle} from "@headlessui/react";
+import {XMarkIcon} from "@heroicons/react/24/outline";
 
 interface DialogFormsProps {
   id: number;
@@ -106,6 +107,18 @@ export default function DialogForms({id, isOpen, onClose}: DialogFormsProps) {
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             className="relative transform overflow-hidden rounded-lg bg-white px-6 py-5 text-left shadow-xl sm:w-full sm:max-w-lg">
+            {/* Close button added here */}
+            <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block">
+              <button
+                type="button"
+                onClick={onClose}
+                className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="sr-only">Close</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+
             <div>
               <DialogTitle as="h3" className="text-base font-semibold text-gray-900">
                 Answer the Question
@@ -160,12 +173,15 @@ export default function DialogForms({id, isOpen, onClose}: DialogFormsProps) {
                 >
                   Submit
                 </button>
-                <button
-                  onClick={handleDelete}
-                  className="inline-flex items-center gap-x-1.5 rounded-md bg-red-600 ml-2 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500"
-                >
-                  Delete
-                </button>
+                {/* In create question, there is no delete button */}
+                {id !== 0 && (
+                  <button
+                    onClick={handleDelete}
+                    className="inline-flex items-center gap-x-1.5 rounded-md bg-red-600 ml-2 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500"
+                  >
+                    Delete
+                  </button>
+                )}
               </div>
             </div>
           </DialogPanel>
